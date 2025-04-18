211a6f40d061e09fdfa63279a5a265b5965eb30b
ApeEgg1 <pvpskillplayer@gmail.com>
2025-04-18 19:26:50 +0300

    Update get-tiles.js

--- ApeLegend/netlify/functions/get-tiles.js
+++ ApeLegend/netlify/functions/get-tiles.js
@@ -1,10 +1,6 @@
 
 const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 exports.handler = async () => {
   try {
